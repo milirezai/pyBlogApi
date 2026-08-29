46ec84faf2ce5515f0312a3ec4452dfafbb4c6db
milirezai <miladrezaix@gmail.com>
2026-08-29 16:20:14 +0330

    connect to database sqlite

---
+++ app/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+DATABASE_URL = "sqlite:///.blog.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()        
+
+class base(DeclarativeBase):
+    pass
--- app/main.py
+++ app/main.py
@@ -4,12 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return "hello fast api"
-
-@app.get("/mili")
-def mili():
-    return 'mili in fast api'
-
-@app.get('/post')
-def post():
-    return 'hi post'
+    return "blog with fastApi"
